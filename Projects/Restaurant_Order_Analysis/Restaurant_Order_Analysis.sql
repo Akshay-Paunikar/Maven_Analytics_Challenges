ad5254738f083b05d3823b12f40740eb8fe860e6
Akshay Paunikar <akshayakki29@gmail.com>
2025-03-18 16:58:45 +0530

    Restaurant Order Analysis - Objective 1 -  Task 1 done

--- Projects/Restaurant_Order_Analysis/Restaurant_Order_Analysis.sql
+++ Projects/Restaurant_Order_Analysis/Restaurant_Order_Analysis.sql
@@ -1,2 +1,11 @@
+/* RESTAURANT ORDER ANALYSIS MYSQL PROJECT*/
 USE restaurant_db;
 
+-- Objective 1: Explore the items table --
+-- Your first objective is to better understand the items table by finding the number of rows in the table, the least and most expensive items, and the 
+-- item prices within each category.
+-- 1. View the menu_items table and write a query to find the number of items on the menu --
+SELECT * FROM menu_items;
+
+SELECT COUNT(menu_items.menu_item_id) AS num_of_items
+FROM menu_items;
